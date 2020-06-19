4f53052e00efc56ab4d0788fba22b42f3d9e9342
Kathleen Tonner <kathleentonner@gmail.com>
2020-06-19 14:55:41 -0400

    routers added

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Prescriptions from './Pages/Prescriptions'
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch
+} from 'react-router-dom';
 
 function App() {
   return (
@@ -8,15 +16,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mental Health Tracker
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          to='/enter'
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get Started
         </a>
       </header>
     </div>
