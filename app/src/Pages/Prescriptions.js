a633aece7413e3a8f70bad63af6fe23aed005f30
Kathleen Tonner <kathleentonner@gmail.com>
2020-06-22 16:24:18 -0400

    calendar started

--- app/src/Pages/Prescriptions.js
+++ app/src/Pages/Prescriptions.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col' 
 import Input from '../components/Input'
 import css from './Prescriptions.css'
+import Calendar from '../components/Calendar/Calendar'
 
 function Prescriptions () {
     return(
@@ -10,6 +11,7 @@ function Prescriptions () {
         <Row>
         <Col className="mx-auto">
         <Input/>
+        <Calendar/>
         </Col>
         </Row>
 
---
+++ app/src/components/Calendar/Calendar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import moment from 'moment'
+import Table from 'react-bootstrap/Table'
+import css from './style.css'
+
+function Calendar(){
+    const weekdayshort = moment.weekdaysShort();
+    const weekdayshortname = weekdayshort.map(day => {
+        return (
+          <th key={day} className="week-day">
+           {day}
+          </th>
+        );
+     });
+     return(
+      <Table>
+         {weekdayshortname}
+      </Table>
+     )
+}
+
+export default Calendar
