b71c1b5e25a5a0dea7534f7ab13197c333ff0d05
unknown <kathleentonner@gmail.com>
2020-10-08 23:29:30 -0400

    Updates

---
+++ precache-manifest.fc0f2359e8259b61ed2bef6b124980ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1533d1ca8c3121a58741af8478ff2993",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60e85385851303e8a758",
+    "url": "/static/css/main.0a9da7d7.chunk.css"
+  },
+  {
+    "revision": "23aac19ebe16d6c54ff3",
+    "url": "/static/js/2.ab6eeb6b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.ab6eeb6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e85385851303e8a758",
+    "url": "/static/js/main.c2dcf30b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
